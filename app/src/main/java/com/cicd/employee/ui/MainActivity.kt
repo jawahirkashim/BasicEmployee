5588db9c591ac3c503c00beaa245f8da4392b191
jawahir <jawahir.abul2007@gmail.com>
2023-06-21 17:04:03 +0400

    Init

---
+++ app/src/main/java/com/cicd/employee/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cicd.employee.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cicd.employee.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
